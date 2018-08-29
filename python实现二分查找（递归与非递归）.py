8fea22c6b0c20a85e64802cc319b86994aa14bc2
wuzhiyang <850781645@qq.com>
2018-08-29 17:11:40 +0800

    01

--- python实现二分查找（递归与非递归）.py
+++ python实现二分查找（递归与非递归）.py
@@ -38,13 +38,13 @@
         
         if left > right: #递归结束条件
             return -1
-            mid = (left + right) // 2
-            if num < lis[mid]:
-                right = mid -1
+        mid = (left + right) // 2
+        if num < lis[mid]:
+            right = mid -1
         elif num > lis[mid]:
             left = mid + 1
-            else:
-                return mid
+        else:
+            return mid
         return binary_search(lis, left, right, num)
         #这里之所以会有return是因为必须要接收值，不然返回None
         #回溯到最后一层的时候，如果没有return，那么将会返回None
