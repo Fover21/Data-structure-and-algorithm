820d0819fa82005be28db05e67be2fcc125eac21
wuzhiyang <850781645@qq.com>
2018-12-25 17:05:09 +0800

    list

---
+++ link_list/list.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+# __author: ward
+# data: 2018/12/25
+# @File: list
+
+
+class Node(object):
+    def __init__(self, initdata):
+        self.data = initdata
+        self.next = next
+
+    def get_data(self):
+        return self.data
+
+    def get_next(self):
+        return self.next
+
+    def set_data(self, new_data):
+        self.data = new_data
+
+    def set_next(self, new_next):
+        self.next = new_next
+
+
+# Unordered_list
+class UnorderedList(object):
+
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head == None
+
+    def add(self, item):
+        temp = Node(item)
+        temp.set_next(self.head)
+        self.head = temp
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count += 1
+            current = current.get_next()
+        return count
+
+    def search(self, item):
+        current = self.head
+        found = False
+        while current != None and not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                current = current.get_next()
+        return found
+
+    def remove(self, item):
+        current = self.head
+        previous = None
+        found = False
+        while not found:
+            if current.get_data() == item:
+                found = True
+            else:
+                previous = current
+                current = current.get_next()
+        if previous == None:
+            self.head = current.get_next()
+        else:
+            previous.set_next(current.get_next())
+
+
+# 构建一个空的链表
+my_list = UnorderedList()
+my_list.add(3)
+my_list.add(17)
+
+print(my_list.size())
+print(my_list.search(17))
+my_list.remove(3)
+print(my_list.size())
---
+++ test.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# __author: ward
+# data: 2018/12/25
+# @File: test
+
+
+# s = iter([1, 2, 3])
+# print(s.__next__())
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+# 头插法 （带头节点的方式）
+def create(li):
+    head = Node(None)
+    for num in li:
+        p = Node(num)
+        p.next = head.next
+        head.next = p
+    return head
+
+
+# 打印
+def print_list(head):
+    p = head.next
+    while p:
+        print(p.data)
+        p = p.next
+
+
+head = create([1, 2, 3, 4, 5])
+print_list(head)
